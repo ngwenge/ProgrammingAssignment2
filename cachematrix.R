f08ce17cdc73430c0a3f135a7069711ba36c1605
ngwenge <ng.wge073@gmail.com>
2014-05-25 22:40:17 +0800

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Basically, there are two "parent" functions being created, 'makeCacheMatrix', the one that 
+## gets and caches the matrix; 'cacheSolve', the one that execute the inverse matrix function. 
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' basically consists of four "child"/subfunctions, which are 'set', 'get', 
+## 'setinverse' and 'getinverse' functions and these function generally help to cache the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' is executing the 'solve' function which provides the inverse of the matrix from
+## 'makeCacheMatrix'. If a matrix is executed once with this function, the result will be cached 
+## it will print out the message "getting cached data" together with the answer when we execute 
+## the same matrix for the second time (or more). 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
